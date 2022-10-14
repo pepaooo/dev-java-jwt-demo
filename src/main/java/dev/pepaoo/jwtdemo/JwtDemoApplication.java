b8dc954551cd0ae6060c9377135c05544471a15d
Jose Luis Angulo <pepaoo@hotmail.com>
2022-10-14 07:24:45 -0500

    Spring Security JWT: How to secure your Spring Boot REST APIs with JSON Web Tokens

---
+++ src/main/java/dev/pepaoo/jwtdemo/JwtDemoApplication.java
@@ -0,0 +1,16 @@
+package dev.pepaoo.jwtdemo;
+
+import dev.pepaoo.jwtdemo.config.RsaKeyProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@EnableConfigurationProperties(RsaKeyProperties.class)
+@SpringBootApplication
+public class JwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/pepaoo/jwtdemo/controller/HomeControllerTest.java
@@ -0,0 +1,51 @@
+package dev.pepaoo.jwtdemo.controller;
+
+import dev.pepaoo.jwtdemo.config.SecurityConfig;
+import dev.pepaoo.jwtdemo.service.TokenService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest({HomeController.class, AuthController.class})
+@Import({SecurityConfig.class, TokenService.class})
+class HomeControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void rootWhenUnauthenticatedThen401() throws Exception {
+        this.mvc.perform(get("/"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void rootWhenAuthenticatedThenSaysHelloUser() throws Exception {
+        MvcResult result = this.mvc.perform(post("/token")
+                        .with(httpBasic("pepaoo", "password")))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String token = result.getResponse().getContentAsString();
+
+        this.mvc.perform(get("/")
+                        .header("Authorization", "Bearer " + token))
+                .andExpect(content().string("Hello, pepaoo"));
+    }
+
+    @Test
+    @WithMockUser
+    public void rootWithMockUserStatusIsOK() throws Exception {
+        this.mvc.perform(get("/")).andExpect(status().isOk());
+    }
+}
